4825eba97d76375f2c4b0ec613089dac75fd6056
Politrees <143968312+Bebra777228@users.noreply.github.com>
2025-04-15 23:26:15 +0500

    CPU support

--- Example.py
+++ Example.py
@@ -10,7 +10,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 student_ldm_ckpt_path:str = './ModelWeights/student_ldm.pth'
 sr_vocoder_ckpt_path:str = './ModelWeights/sr_vocoder.pth'
 vae_ckpt_path:str = './ModelWeights/vae.pth'
-flashsr = FlashSR( student_ldm_ckpt_path, sr_vocoder_ckpt_path, vae_ckpt_path)
+flashsr = FlashSR( student_ldm_ckpt_path, sr_vocoder_ckpt_path, vae_ckpt_path, device)
 flashsr = flashsr.to(device)
 
 audio_path:str = './Assets/ExampleInput/music.wav'
